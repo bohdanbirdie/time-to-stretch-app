c8b846e733612e72bd4425dd3b8ba73678b2fd99
Bohdan Ptyts <bohdanptyts@gmail.com>
2025-05-01 19:49:56 +0200

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Bohdan Ptyts on 01.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerTests/TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTests.swift
+//  TimerTests
+//
+//  Created by Bohdan Ptyts on 01.05.2025.
+//
+
+import Testing
+@testable import Timer
+
+struct TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
